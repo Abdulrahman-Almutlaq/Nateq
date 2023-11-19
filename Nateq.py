c3dfb0b6395bc29f7a53d7025a6c8f9ec5d3642a
Abdulrahman <dahoomi.ibr.mut@gmail.com>
2023-11-19 13:15:54 +0300

    oo

--- Nateq.py
+++ Nateq.py
@@ -134,4 +134,4 @@ demo_1 = gr.Interface(fn=audio_gen_1,
 
 demo = gr.TabbedInterface([demo_1, demo_2, demo_3], ["Faster model", "Better model", "Record!"])
 
-demo.launch(inline = False, share=True)
+demo.launch(inline = False, server='0.0.0.0')
