3907d7d430e716c276912f4833f0cd9b8827b339
Abdulrahman Almutlaq <53004325+Abdulrahman-Almutlaq@users.noreply.github.com>
2023-11-19 06:19:27 +0300

    message

--- Nateq.py
+++ Nateq.py
@@ -70,7 +70,7 @@ def record_and_thank_you(word_recording, word):
     return f"Thank you for recording the word: {word}!"
 
 demo_3 = gr.Interface(fn=record_and_thank_you, 
-                         inputs=[gr.Microphone(text="Say a word:"), 'text'],
+                         inputs=[gr.Microphone(text="Say a word:"), gr.Text()],
                          outputs="text")
 
 
